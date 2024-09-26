693f0989945f0a39bee4aa2b6f8b24928275ce0a
Will <will@kentik.com>
2024-09-26 10:28:58 +0900

    improve memory efficiency of dns records

--- src/dns/mod.rs
+++ src/dns/mod.rs
@@ -1,35 +1,50 @@
-use serde::{Serialize, Deserialize};
-
-pub mod client;
+use std::net::IpAddr;
+use smallvec::SmallVec;
+use serde::{Serialize, Serializer};
 
 pub use client::Client;
 
-#[derive(Clone, Eq, PartialEq, Serialize, Deserialize, Debug)]
+#[derive(Clone, Eq, PartialEq, Serialize, Debug)]
 pub struct Response {
     #[serde(rename = "Question")]
     pub question: Question,
     #[serde(rename = "Answers")]
-    pub answers:  Vec<Answer>,
+    pub answers:  SmallVec<[Answer; 1]>,
 }
 
-#[derive(Clone, Eq, PartialEq, Serialize, Deserialize, Debug)]
+#[derive(Clone, Eq, PartialEq, Serialize, Debug)]
 pub struct Question {
     #[serde(rename = "Name")]
     pub name: String,
-    #[serde(rename = "Host", with = "serde_bytes")]
-    pub host: Vec<u8>,
+    #[serde(rename = "Host")]
+    pub host: Address,
     #[serde(rename = "Port")]
     pub port: u16,
 }
 
-#[derive(Clone, Eq, PartialEq, Serialize, Deserialize, Debug)]
+#[derive(Clone, Eq, PartialEq, Serialize, Debug)]
 pub struct Answer {
     #[serde(rename = "Name")]
     pub name:  String,
     #[serde(rename = "CNAME")]
     pub cname: String,
-    #[serde(rename = "IP", with = "serde_bytes")]
-    pub ip:    Vec<u8>,
+    #[serde(rename = "IP")]
+    pub ip:    Address,
     #[serde(rename = "TTL")]
     pub ttl:   u32,
 }
+
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct Address(pub IpAddr);
+
+impl Serialize for Address {
+    fn serialize<S: Serializer>(&self, serializer: S) -> Result<S::Ok, S::Error> {
+        match self.0 {
+            IpAddr::V4(ip) => serializer.serialize_bytes(&ip.octets()[..]),
+            IpAddr::V6(ip) => serializer.serialize_bytes(&ip.octets()[..]),
+        }
+    }
+}
+
+pub mod client;
+
